308847d6d2107c6e803a0289f8938dc12df69143
JamesFether <48078054+JamesFether@users.noreply.github.com>
2019-05-06 12:00:54 +1000

    Challenge 1

---
+++ Challenge 1/Challenge 1/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Challage_One_Roll_the_Dice
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            string again = "yes";
+            while (again == "yes")
+
+            {
+                Console.WriteLine(string.Concat(Enumerable.Repeat("-", Console.BufferWidth - 1)));
+                Console.WriteLine("Hey! Lets play a game!");
+                Console.WriteLine("Roll the Dice");
+
+                String user = Console.ReadLine();
+
+                Random r = new Random();
+                int user1 = r.Next(3);
+
+
+                if (user1 == 1)
+                {
+                    if (user == "Rolls")
+                    {
+                        Console.WriteLine("The User Rolled the dice");
+                        Console.WriteLine("Its a 1!");
+                    }
+
+                }
+            }
+
+        }
+    }
+
+}
